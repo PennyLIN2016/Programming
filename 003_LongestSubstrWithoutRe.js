c5ff197973eda5313d284b21ecce49765c204e95
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2019-03-04 22:32:02 +1100

    Update 003_LongestSubstrWithoutRe.js

--- 003_LongestSubstrWithoutRe.js
+++ 003_LongestSubstrWithoutRe.js
@@ -7,15 +7,18 @@ var lengthOfLongestSubstring = function(s) {
     if (s.length === 0){
         return 0;
     }
-    var d = {};
-    var max_len= 0;
-    var start = 0;
+    var d = {};// save the existing chars in s. char:i i is the last pos in s. 
+    var max_len= 0;// current max length
+    var start = 0;// the start pos of the current compared sub string
     var i = 0;
     for (i=0;i<s.length;i++){
-        if (d[s[i]]!==undefined){
+        if (d[s[i]]!==undefined){// found a repeated char
+            // the new compared sub string starts from the next char of the repeated pos.
             start = Math.max(start,d[s[i]]+1);
         };
+        // refresh the last pos of the char
         d[s[i]]= i;
+        //compare currentsub string to the max  len.  
         max_len = Math.max(max_len,i-start+1);
 
     };
