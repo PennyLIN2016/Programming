a4fedcde04781bce84d695f106cdf95d5390fdde
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2019-03-08 14:41:45 +1100

    Update 014_LongPrefix.py

--- 014_LongPrefix.py
+++ 014_LongPrefix.py
@@ -1,16 +1,18 @@
 
-class question(object):
+class Solution(object):
 
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
         :rtype: str
         """
+        if len(strs)==0:
+            return ''
         tmp_pre = strs[0]
         for i in range (1,len(strs)):
             while strs[i].find(tmp_pre)!= 0:
                 if len(tmp_pre)>1:
-                        tmp_pre = tmp_pre[:-1]
+                    tmp_pre = tmp_pre[:-1]
                 else:
                     return ''
         return tmp_pre
@@ -19,7 +21,7 @@ class question(object):
 if __name__ == '__main__':
 
     inputs = ['','','','']
-    k = question()
+    k = Solution()
     print 'Input :', inputs
     r = k.longestCommonPrefix(inputs)
     print  'Result : ',r
