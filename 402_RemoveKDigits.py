3694ce803430ca6ee611e14835a7055db8f99a45
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2019-08-05 20:58:20 +1000

    Add files via upload

---
+++ 402_RemoveKDigits.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        #Runtime: 36 ms, faster than 56.45% of Python online submissions for Remove K Digits.
+        #Memory Usage: 11.9 MB, less than 92.68% of Python online submissions for Remove K Digits.
+        # other`s solution: time : o(n) space o(n)
+        if not num or len(num)==k: return '0'
+        stack=[]
+        for value in num:
+            #make sure to keep the old order
+            # if the number is greater than the later one , remove the front one
+            while stack and k and int(stack[-1])>int(value):
+                stack.pop()
+                k-=1
+            stack.append(value)
+        # k>1: the left numbers is in increasing order already
+        # romove the greater ones
+        while k:
+            stack.pop()
+            k-=1
+        return str(int(''.join(stack)))
+
+
+
+
+if __name__ == '__main__':
+    object = Solution()
+    num = "912000348567"
+    k = 4
+
+    print(num)
+    r = object.removeKdigits(num,k)
+    print(r)
+    print('---End---')
---
+++ 404_SumOfLeftLeaves.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        #Runtime: 20 ms, faster than 73.79% of Python online submissions for Sum of Left Leaves.
+        #Memory Usage: 12.1 MB, less than 91.67% of Python online submissions for Sum of Left Leaves.
+        # time: 0(n)  space:o(n)
+        if not root: return 0
+        res=0
+        stack=[(root,0)]
+        while stack:
+            tmp,flag=stack.pop()
+            # only accumulate the left leaves
+            if flag and not tmp.left and not tmp.right:
+                res+=tmp.val
+            if tmp.left:stack.append((tmp.left,1))
+            if tmp.right:stack.append((tmp.right,0))
+        return res
+
+if __name__ == '__main__':
+    object = Solution()
+    n1=TreeNode(3)
+    n2=TreeNode(9)
+    n3=TreeNode(20)
+    n4=TreeNode(15)
+    n5=TreeNode(7)
+    """
+    n1.left=n2
+    n1.right=n3
+    n3.left=n4
+    n3.right=n5
+    """
+
+    r = object.sumOfLeftLeaves(n1)
+    print(r)
+    print('---End---')
