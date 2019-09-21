6eb76167f41fa907b8f28b442bede33f77f82921
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2019-09-21 22:00:01 +1000

    Add files via upload

---
+++ 486_PredictTheWinner.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def PredictTheWinner(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        #Runtime: 20 ms, faster than 74.68% of Python online submissions for Predict the Winner.
+        #Memory Usage: 11.6 MB, less than 100.00% of Python online submissions for Predict the Winner.
+        total=sum(nums)
+        print total
+        self.fD,self.sD={},{}
+        one=self.f(nums,0,len(nums)-1)
+        print one
+        print self.sD
+        print self.fD
+        return one*2 >=total
+    def f(self,nums,start,end):
+        # get the nums and get the possible maximum scores
+        # for play 2 action
+        if start==end:
+            return nums[start]
+        # in case the combination have been covered from the other direction.
+        if(start,end) not in self.fD:
+            self.fD[(start,end)]=max(nums[start]+self.s(nums,start+1,end),nums[end]+self.s(nums,start,end-1))
+        return self.fD[(start,end)]
+
+    def s(self,nums,start,end):
+        # do not get the num and get the possible miximum scores
+        # for play2 action
+        if start==end:
+            return 0
+        if(start,end) not in self.sD:
+            self.sD[(start,end)]=min(self.f(nums,start+1,end),self.f(nums,start,end-1))
+        return self.sD[(start,end)]
+
+    def PredictTheWinner1(self, nums):
+        #Runtime: 28 ms, faster than 22.51% of Python online submissions for Predict the Winner.
+        #Memory Usage: 11.7 MB, less than 100.00% of Python online submissions for Predict the Winner.
+        # dynamic programming solution:
+        dp = [0]*len(nums)
+        for i in range(len(nums)-1,-1,-1):
+            for j in range(i+1,len(nums)):
+                dp[j]=max(nums[i]-dp[j],nums[j]-dp[j-1])
+        return dp[-1]>=0
+
+if __name__ == '__main__':
+    object = Solution()
+    A= [1,3,1]
+
+    print('---Start---')
+    r = object.PredictTheWinner(A)
+    print(r)
+    print('---End---')
