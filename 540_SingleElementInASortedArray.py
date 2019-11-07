b0035803987c1413ee86f0c6ee13b02831e1ef38
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2019-11-07 20:41:46 +1100

    Update 540_SingleElementInASortedArray.py

--- 540_SingleElementInASortedArray.py
+++ 540_SingleElementInASortedArray.py
@@ -13,6 +13,17 @@ class Solution(object):
         for i in range(2,len(nums)):
             res^=nums[i]
         return res
+    
+    def singleNonDuplicate(self, nums):
+        # Runtime: 60 ms, faster than 35.77% of Python online submissions for Single Element in a Sorted Array.
+        #Memory Usage: 13.6 MB, less than 7.14% of Python online submissions for Single Element in a Sorted Array.
+        # my own solution: timeo(n/2)
+        #if not nums or len(nums)%2==0 :return None
+        if len(nums)==1:return nums[0]
+        for i in range(0,len(nums),2):
+            if i == len(nums)-1:return nums[i]
+            if nums[i]!=nums[i+1]:
+                return nums[i]
 
 
 if __name__ == '__main__':
