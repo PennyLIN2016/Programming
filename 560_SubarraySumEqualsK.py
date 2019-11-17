85f1802f9c2ea32f15e4536337bf5ebddf781330
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2019-11-18 08:45:13 +1100

    Add files via upload

---
+++ 560_SubarraySumEqualsK.py
@@ -0,0 +1,52 @@
+import collections
+class Solution(object):
+    def subarraySum1(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        # 58 / 80 test cases passed. time out
+        # brutal solution: o(n*n) space :o(1)
+        if not nums: return 0
+        l=len(nums)
+        res=0
+        for i in range(l):
+            curSum=0
+            for j in range(i,l):
+                curSum+=nums[j]
+                if curSum==k: res+=1
+        return res
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        #Runtime: 88 ms, faster than 90.97% of Python online submissions for Subarray Sum Equals K.
+        #Memory Usage: 14.2 MB, less than 8.00% of Python online submissions for Subarray Sum Equals K.
+        # google solution: time : o(n) space :o(n)
+        # dp[key] store the occurence times of  the sum[:pos-1] equals to key// sum[i:j] = sum[:j]-sum[:i]
+        dp=collections.defaultdict(int)
+        dp[0]=1
+        curSum,res=0,0
+        for i in range(len(nums)):
+            curSum+=nums[i]
+            # curSum- key=k so sum[x:i+1] = k
+            if curSum-k in dp:
+                res+=dp[curSum-k]
+            dp[curSum]+=1
+        return res
+
+
+if __name__ == '__main__':
+    object = Solution()
+    n1=[1,1,1]
+    k = 2
+
+
+    print('---Start---')
+
+    r = object.subarraySum(n1,k)
+    print(r)
+    print('---End---')
---
+++ 561_ArrayPartition1.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def arrayPairSum1(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #Runtime: 264 ms, faster than 47.51% of Python online submissions for Array Partition I.
+        #Memory Usage: 14.2 MB, less than 6.06% of Python online submissions for Array Partition I.
+        # my own solution: time : o(nlgn) space : o(1)
+        nums.sort()
+        res=0
+        for i in range(0,len(nums),2):
+            res+=nums[i]
+        return res
+
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #Runtime: 244 ms, faster than 97.95% of Python online submissions for Array Partition I.
+        #Memory Usage: 14 MB, less than 69.70% of Python online submissions for Array Partition I.
+        # the same thought to the solution 1: simple syntax more efficient.
+        nums.sort()
+        return sum(nums[::2])
+
+if __name__ == '__main__':
+    object = Solution()
+    n1=[1,4,3,2]
+
+
+    print('---Start---')
+
+    r = object.arrayPairSum(n1)
+    print(r)
+    print('---End---')
