84d2a0d0b8ca3d6feac9b4c8ea74c5f8e93f68a8
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-03-12 09:39:03 +1100

    Update 397_IntergerReplacement.py

--- 397_IntergerReplacement.py
+++ 397_IntergerReplacement.py
@@ -1,4 +1,5 @@
 class Solution:
+    # the dp[i] is decided by dp[i-1] and dp[i+1], so dp is not workable solution
     def integerReplacement(self, n: int) -> int:
         # Runtime: 32 ms, faster than 85.00% of Python3 online submissions for Integer Replacement.
         # Memory Usage: 13.8 MB, less than 11.11% of Python3 online submissions for Integer Replacement.\
@@ -16,6 +17,18 @@ class Solution:
             else:
                 n-=1
         return res
+    def integerReplacement2(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        #Runtime: 215 ms, faster than 36.19% of Python online submissions for Integer Replacement.
+        #Memory Usage: 13.4 MB, less than 71.43% of Python online submissions for Integer Replacement.
+        if n == 1: return 0
+        if n%2 == 0:
+            return self.integerReplacement(n>>1)+1
+        else:
+            return min(self.integerReplacement(n+1), self.integerReplacement(n-1)) + 1
 
 if __name__ == '__main__':
 
