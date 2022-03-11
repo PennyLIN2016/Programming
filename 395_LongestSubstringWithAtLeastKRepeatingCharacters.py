0ed62b426ff26cfc61274a715ec160572dcf1832
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-03-11 16:16:51 +1100

    Update 395_LongestSubstringWithAtLeastKRepeatingCharacters.py

--- 395_LongestSubstringWithAtLeastKRepeatingCharacters.py
+++ 395_LongestSubstringWithAtLeastKRepeatingCharacters.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         #Runtime: 16 ms, faster than 96.07% of Python online submissions for Longest Substring with At Least K Repeating Characters.
         #Memory Usage: 11.8 MB, less than 97.50% of Python online submissions for Longest Substring with At Least K Repeating Characters.
-        # time : o(n) space: o(1)
+        
         def sublongestStr(s,k,left,right):
             charCount=[0]*26
             # the count in s[left:right],not in s[pos1:pos2], so need to check the sub str in next recursion
@@ -28,7 +28,23 @@ class Solution(object):
                 pos1=pos2
             return maxLen
         return sublongestStr(s,k,0,len(s))
-
+    
+    def longestSubstring2(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        # Runtime: 20 ms, faster than 93.41% of Python online submissions for Longest Substring with At Least K Repeating Characters.
+        # Memory Usage: 13.7 MB, less than 54.40% of Python online submissions for Longest Substring with At Least K Repeating Characters.
+        if k > len(s): return 0
+        for c in s:
+            # if str is the target string is decided by the less occurance char in the string. 
+            # if count c less than k, delect the c and check the sub strings splited by the letter.
+            if s.count(c)< k:
+                return(max(self.longestSubstring(t,k) for t in s.split(c)))
+        # all chars in string count more than k
+        return len(s)
 
 if __name__ == '__main__':
     object = Solution()
