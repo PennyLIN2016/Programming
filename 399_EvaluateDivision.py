615c47c9941981d97b2a24f1be15eff0f01bb301
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-03-12 14:49:50 +1100

    Update 399_EvaluateDivision.py

--- 399_EvaluateDivision.py
+++ 399_EvaluateDivision.py
@@ -1,6 +1,6 @@
 import collections
 class Solution(object):
-    def calcEquation(self, equations, values, queries):
+    def calcEquation1(self, equations, values, queries):
         """
         :type equations: List[List[str]]
         :type values: List[float]
@@ -28,7 +28,33 @@ class Solution(object):
             table[y][x] = 1.0 / value
         res = [dfs(x,y,table,set()) if x in table and y in table else -1.0 for (x,y) in queries]
         return res
-
+    def calcEquation(self, equations, values, queries):
+        """
+        :type equations: List[List[str]]
+        :type values: List[float]
+        :type queries: List[List[str]]
+        :rtype: List[float]
+        """
+        #Runtime: 26 ms, faster than 44.29% of Python online submissions for Evaluate Division.
+        #Memory Usage: 13.5 MB, less than 51.84% of Python online submissions for Evaluate Division.
+        import collections
+        g = collections.defaultdict(lambda: collections.defaultdict(int))
+        visited = set()
+        for (x,y), v in zip(equations, values):
+            g[x][y] = v
+            g[x][y] = 1.0/v
+            visited.add(x)
+            visited.add(y)
+        for i in visited:
+            g[i][i] = 1.0
+            for j in visited:
+                for k in visited:
+                    if g[j][i] and g[i][k]:
+                        g[j][k] = g[j][i] *  g[i][k]
+        res = []
+        for i, j in queries:
+            res.append(g[i][j] if g[i][j] else -1.0)
+        return res
 
 if __name__ == '__main__':
     object = Solution()
