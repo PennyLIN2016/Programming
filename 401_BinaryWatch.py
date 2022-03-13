25880f2c6050d99f3834297bc988f0fa52384969
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-03-13 16:23:54 +1100

    Update 401_BinaryWatch.py

--- 401_BinaryWatch.py
+++ 401_BinaryWatch.py
@@ -20,7 +20,57 @@ class Solution(object):
         res=[]
         dfs(num,0,res)
         return res
+    
+    def readBinaryWatch2(self, turnedOn):
+        """
+        :type turnedOn: int
+        :rtype: List[str]
+        """
+        #Runtime: 24 ms, faster than 65.06% of Python online submissions for Binary Watch.
+        #Memory Usage: 13.8 MB, less than 15.66% of Python online submissions for Binary Watch.
+        def dfs(lightNum,s,sum, visited):
+            if lightNum == 0:
+                return
+            for i in [1,2,4,8,16,32]:
+                if i in visited:
+                    continue
+                if sum + i > 59:
+                    return
+                if lightNum == 1:
+                    if sum+i not in s:
+                        s.append(sum+i)
+                        continue
+                else:
+                    visited.add(i)
+                    dfs(lightNum-1, s, sum+i, visited)
+                    visited.remove(i)
 
+
+        if turnedOn in [9,10]:
+            return []
+        hours = {
+            0: ["0"],
+            1: ["1", '2', '4', '8'],
+            2: ["9", '10', "6", '5', '3'],
+            3: ["7", '11' ],
+        }
+        sTurned = min(5, turnedOn)
+        seconds = {0: ['00']}
+        for i in range(1, sTurned+1):
+            second = []
+            visitedSet = set()
+            dfs(i,second,0, visitedSet)
+            seconds[i] = second
+        #print(seconds)
+        res = []
+        for t in range(sTurned+1):
+            h = turnedOn -t
+            if h< 0 or h > 3:
+                continue
+            for i in hours[h]:
+                for j in seconds[t]:
+                    res.append('{}:{}'.format(i, str(j).zfill(2)))
+        return res
 
 
 if __name__ == '__main__':
