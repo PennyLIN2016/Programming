90db3116140bf413ab1db8222f87198bb3491ec6
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-03-16 09:52:09 +1100

    Update 407_LongestPalindrom.py

--- 407_LongestPalindrom.py
+++ 407_LongestPalindrom.py
@@ -16,7 +16,25 @@ class Solution(object):
                 flag=0
             res+=int(nums[v]/2)*2
         return res
-
+    def longestPalindrome1(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        #Runtime: 41 ms, faster than 21.54% of Python online submissions for Longest Palindrome.
+        #Memory Usage: 13.4 MB, less than 87.79% of Python online submissions for Longest Palindrome.
+        import collections
+        c = collections.Counter(s)
+        flag = 0
+        res = 0
+        for v in c.values():
+            res += v
+            if v%2 != 0:
+                if flag ==0:
+                    flag = 1
+                else:
+                    res -= 1
+        return res
 
 if __name__ == '__main__':
     object = Solution()
