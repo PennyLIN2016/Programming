95498a597acf6ff6495199da5101cfe24e54f0d3
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-03-20 19:45:58 +1100

    Update 424_LongestRepeatingCharacterReplacement.py

--- 424_LongestRepeatingCharacterReplacement.py
+++ 424_LongestRepeatingCharacterReplacement.py
@@ -18,6 +18,7 @@ class Solution(object):
             fast += 1
             map_list[ord(cur_fast)-ord('A')]+=1
             # the maxLen comes from map_list[], so will not be greater than the real maxlen of repeating letter
+            # get most common vlaue count
             maxLen=max(maxLen,map_list[ord(cur_fast)-ord('A')])
             #the gap can not be covered by k
             while(fast-slow-maxLen>k):
@@ -28,7 +29,33 @@ class Solution(object):
 
         return res
 
-
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        # Runtime: 594 ms, faster than 5.02% of Python online submissions for Longest Repeating Character Replacement.
+        # Memory Usage: 13.7 MB, less than 70.30% of Python online submissions for Longest Repeating Character Replacement.
+        # sliding window problem: two pointer solution
+        # time: o(n) space: o(n)
+        n = len(s)
+        # an empty counter object
+        c = collections.Counter()
+        res = 0
+        # two pointer, [left, right] is the sub string required
+        left, right = 0,0
+        while right< n:
+            c[s[right]]+=1
+            # can't meet the requirement
+            # downsize the window to ensure the window satisfied.
+            while right-left+1 -c.most_common(1)[0][1]>k:
+                c[s[left]] -= 1
+                left += 1
+            # update the current max result
+            res = max(res, right-left+1)
+            right += 1
+        return res
 
 
 if __name__ == '__main__':
