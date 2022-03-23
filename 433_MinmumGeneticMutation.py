eb5950746c93d5572be0135388cf4492c21eca85
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-03-23 19:57:00 +1100

    Update 433_MinmumGeneticMutation.py

--- 433_MinmumGeneticMutation.py
+++ 433_MinmumGeneticMutation.py
@@ -25,6 +25,49 @@ class Solution(object):
                     res= tmp
                     break
         return  res
+    def minMutation(self, start, end, bank):
+        """
+        :type start: str
+        :type end: str
+        :type bank: List[str]
+        :rtype: int
+        """
+        # Runtime: 16 ms, faster than 87.50% of Python online submissions for Minimum Genetic Mutation.
+        # Memory Usage: 13.5 MB, less than 33.75% of Python online submissions for Minimum Genetic Mutation.
+        # dfs solution:
+
+        # can't use res = float('inf') , res will be a local variant. can't be persist in dfs function
+        res = [float('inf')]
+        if not bank or not end in bank:
+            return -1
+        visited = set()
+
+        def dfs(cur, cnt):
+            for i in range(len(bank)):
+                if bank[i] in visited:
+                    continue
+                if diffNum(bank[i], cur) != 1:
+                    continue
+                if bank[i] == end:
+                    res[0] = min(res[0], cnt + 1)
+                    return
+
+                visited.add(bank[i])
+                dfs(bank[i], cnt + 1)
+                visited.remove(bank[i])
+
+        def diffNum(s1, s2):
+            c = 0
+            for v1, v2 in zip(s1, s2):
+                if v1 != v2:
+                    c += 1
+            return c
+
+        if start in bank:
+            bank.remove(start)
+        dfs(start, 0)
+        return -1 if res[0] == float('inf') else res[0]
+    
 
 if __name__ == '__main__':
     object = Solution()
