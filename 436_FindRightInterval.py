d93ea8b7878c2c566ed132853685293c902badf9
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-03-26 10:33:55 +1100

    Update 436_FindRightInterval.py

--- 436_FindRightInterval.py
+++ 436_FindRightInterval.py
@@ -28,6 +28,53 @@ class Solution(object):
         return res
 
 
+    
+   def findRightInterval(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[int]
+        """
+        # brutal solution: timeout
+        m = len(intervals)
+        if m == 1:
+            return [-1]
+        #print('res-{}'.format(res))
+        tmp = []
+        for i, v in enumerate(intervals):
+            tmp.append([v[0], v[1], i])
+        tmp.sort()
+        print('tmp-{}'.format(tmp))
+        res = [-1] * m
+        for i, v in enumerate(intervals):
+            for j, value in enumerate(tmp):
+                if v[0] == value[0]:
+                    continue
+                if v[1] <= value[0]:
+                    res[i] = value[2]
+                    break
+        return res
+    
+   def findRightInterval(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[int]
+        """
+        # Runtime: 484 ms, faster than 29.09% of Python online submissions for Find Right Interval.
+        # Memory Usage: 19.1 MB, less than 63.64% of Python online submissions for Find Right Interval.
+        # bisect solution time: o(n*nlgn) time: o(n)
+        m = len(intervals)
+        if m == 1:
+            return [-1]
+        # o(nlgn)
+        tmp = sorted((v[0], i, v[1]) for i, v in enumerate(intervals))
+        #print(tmp)
+        res = [-1] * m
+        for v in tmp:
+            # o(nlgn)
+            r = bisect.bisect_left(tmp, (v[2],))
+            if r < m:
+                res[v[1]] = tmp[r][1]
+        return res
 
 
 
