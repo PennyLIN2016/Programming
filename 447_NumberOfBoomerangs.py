2cab3efb720de0bdb95b4d691314c4af7b676571
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-04-06 07:48:30 +1000

    Update 447_NumberOfBoomerangs.py

--- 447_NumberOfBoomerangs.py
+++ 447_NumberOfBoomerangs.py
@@ -20,6 +20,40 @@ class Solution(object):
                 res+=tmpD[d]*(tmpD[d]-1)
         return res
 
+  def numberOfBoomerangs(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        # Runtime: 732 ms, faster than 84.62% of Python online submissions for Number of Boomerangs.
+        # Memory Usage: 41 MB, less than 37.36% of Python online submissions for Number of Boomerangs.
+        # time: o(nlgn) space: o(n**2)
+        import math
+
+        n = len(points)
+        if n < 3: return 0
+        distances = [{} for _ in range(n)]
+        for i in range(n-1):
+            for j in range(i+1, n):
+                # can't work in leecode running, python 2
+                #d = math.dist(points[i], points[j])
+                d = ((points[i][0]-points[j][0])**2 + (points[i][1]-points[j][1])**2)**0.5
+                if d in distances[i]:
+                    distances[i][d] += 1
+                else:
+                    distances[i][d] = 1
+                if d in distances[j]:
+                    distances[j][d] += 1
+                else:
+                    distances[j][d] = 1
+        #print(distances)
+        res = 0
+        for v in distances:
+            for i in v.values():
+                if i > 1:
+                    # A(N,2)
+                    res += i * (i-1)
+        return res
 
 if __name__ == '__main__':
     object = Solution()
