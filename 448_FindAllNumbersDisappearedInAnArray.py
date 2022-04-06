76ce4c60bde4ae82b3033c1e5981fb4c4ae2fcaf
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-04-07 09:08:47 +1000

    Update 448_FindAllNumbersDisappearedInAnArray.py

--- 448_FindAllNumbersDisappearedInAnArray.py
+++ 448_FindAllNumbersDisappearedInAnArray.py
@@ -13,7 +13,24 @@ class Solution(object):
         for i in range(len(nums)):
             if nums[i]>0:res.append(i+1)
         return res
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        #Runtime: 403 ms, faster than 48.50% of Python online submissions for Find All Numbers Disappeared in an Array.
+        # Memory Usage: 20.5 MB, less than 94.99% of Python online submissions for Find All Numbers Disappeared in an Array.
+        # time: o(n) space o(1)
+        res = []
+        n = len(nums)
+        for v in nums:
+            cur = abs(v)
+            nums[cur-1] = abs(nums[cur-1]) * -1
 
+        for i in range(n):
+            if nums[i] > 0:
+                res.append(i+1)
+        return res
 
 if __name__ == '__main__':
     object = Solution()
