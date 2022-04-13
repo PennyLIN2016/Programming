4ddf14e83016905f69bfce5dc0d53bcab825211c
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-04-14 09:21:31 +1000

    Update 453_MiniMumMovesToEqualArrayElements.py

--- 453_MiniMumMovesToEqualArrayElements.py
+++ 453_MiniMumMovesToEqualArrayElements.py
@@ -9,6 +9,22 @@ class Solution(object):
         # to increase n-1 elements by 1 equals to decease the left one element by 1.
         # the time is the gaps for minimum element to others respectively.
         return sum(nums)-min(nums)*len(nums)
+    
+    def minMoves(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Runtime: 209 ms, faster than 92.06% of Python online submissions for Minimum Moves to Equal Array Elements.
+        # Memory Usage: 14.7 MB, less than 39.68% of Python online submissions for Minimum Moves to Equal Array Elements.
+        # Time :o(n) space: o(1)
+        # "In one move, you can increment n - 1 elements of the array by 1" equals to one element decrease by 1
+        # So the solution is the sum of all element gap to the minimum value
+        minValue = min(nums)
+        res = 0
+        for v in nums:
+            res += v - minValue
+        return res
 
 
 
