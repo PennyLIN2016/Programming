0f69480ce81cb84fbed706d0b4d275c165eae0f3
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-04-14 09:54:34 +1000

    Update 454_4Sum2.py

--- 454_4Sum2.py
+++ 454_4Sum2.py
@@ -15,7 +15,51 @@ class Solution(object):
         ab=collections.Counter( a+b for a in A for b in B)
         res= [ab[-c-d] for c in C for d in D]
         return sum(res)
+    def fourSumCount(self, nums1, nums2, nums3, nums4):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type nums3: List[int]
+        :type nums4: List[int]
+        :rtype: int
+        """
+        # Runtime: 487 ms, faster than 94.74% of Python online submissions for 4Sum II.
+        # Memory Usage: 13.9 MB, less than 31.87% of Python online submissions for 4Sum II.
+        # separating solution:
+        # time: o(n**2) space: o(n**2)
+        import collections
+        ct = collections.defaultdict(int)
+        for a in nums1:
+            for b in nums2:
+                ct[a+b] += 1
+        res = 0
+        for c in nums3:
+            for d in nums4:
+                if (c+d) * -1 in ct:
+                    res += ct[(c+d) * -1]
+        return res
 
+
+
+    def fourSumCount1(self, nums1, nums2, nums3, nums4):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type nums3: List[int]
+        :type nums4: List[int]
+        :rtype: int
+        """
+        # brutal solution: timeout: 20/132
+        # time: o(n**4)
+        l = len(nums1)
+        res = 0
+        for i1 in range(l):
+            for i2 in range(l):
+                for i3 in range(l):
+                    for i4 in range(l):
+                        if nums1[i1] + nums2[i2] + nums3[i3] + nums4[i4] == 0:
+                            res += 1
+        return res
 
 
 if __name__ == '__main__':
