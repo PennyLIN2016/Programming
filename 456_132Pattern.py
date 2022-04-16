ce10acbf8d02170930c6f89f17d41cf8add9bd28
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-04-16 12:05:52 +1000

    Update 456_132Pattern.py

--- 456_132Pattern.py
+++ 456_132Pattern.py
@@ -20,6 +20,38 @@ class Solution(object):
             # index of two[-1] < thirds
             two.append(nums[i])
         return False
+    
+   def find132pattern2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        #Runtime: 285 ms, faster than 100.00% of Python online submissions for 132 Pattern.
+        #Memory Usage: 28.5 MB, less than 70.15% of Python online submissions for 132 Pattern.
+        # time: o(n) space: o(n)
+        l = len(nums)
+        if l < 3:
+            return False
+        # left-mid-right: left<right<mid
+        right = float('-inf')
+        # store the value greater than right in order.
+        # every value greater then right and in front of right can work as a mid
+        mid = []
+        for i in range(l-1, -1, -1):
+            # mid has mid and nums[i] works as left
+            # nums[i] < right < mid
+            if nums[i] < right:
+                return True
+            while mid and mid[-1] < nums[i]:
+                # if right<= nums[i] <= mid[-1]: just add a new possible mid value
+                # if mid[-1] < nums[i]: find a new 32
+                # right and mid both greater than left, so the solution is find suitable left
+                # have to iterate in reversed order. 
+                # if using normal order, to find suitable right, right is between [mid, left]
+                # the solution can ensure mid is greater than right. 
+                right = mid.pop()
+            mid.append(nums[i])
+        return False
 
 if __name__ == '__main__':
     object = Solution()
