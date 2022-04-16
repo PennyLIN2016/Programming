667f88b9f72f9b68aa3cbf286cb427142560b3d3
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-04-16 16:10:10 +1000

    Update 459_RepeatedSubstringPattern.py

--- 459_RepeatedSubstringPattern.py
+++ 459_RepeatedSubstringPattern.py
@@ -28,6 +28,24 @@ class Solution(object):
         # in double s, at least one whole s can be found.
         # cannot find the s
         return doubleS.find(s)!=-1
+    
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # Runtime: 76 ms, faster than 58.49% of Python online submissions for Repeated Substring Pattern.
+        # Memory Usage: 14 MB, less than 32.80% of Python online submissions for Repeated Substring Pattern.
+        # brutal solution:
+        # time: 0(n**2) time: o(n)
+        l = len(s)
+        for i in range(1, l//2+1):
+            if l % i != 0:
+                continue
+            tmp = s[0:i]
+            if ''.join((s.split(tmp))) == '':
+                return True
+        return False
 
 
 if __name__ == '__main__':
