55c8c5a1f7803f8f081e0a64d7ceb716ae97e09a
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-05-01 11:15:38 +1000

    Update 481_MagicalString.py

--- 481_MagicalString.py
+++ 481_MagicalString.py
@@ -18,6 +18,22 @@ class Solution(object):
             print"---------"
         return s[:n].count(1)
 
+   def magicalString(self, n: int) -> int:
+        # Runtime: 166 ms, faster than 69.67% of Python3 online submissions for Magical String.
+        # Memory Usage: 15.5 MB, less than 17.21% of Python3 online submissions for Magical String.
+        # time: o(n) space: o(1)
+        m = ['1', '2', '2']
+        index = 2
+        while len(m) < n:
+            #print('index:{} m[index]:{} m[-1]: {}'.format(index, m[index], m[-1]))
+            if m[-1] == '1':
+                m += ['2'] * int(m[index])
+            else:
+                m += ['1'] * int(m[index])
+            index += 1
+            #print(''.join(m))
+
+        return m[:n].count('1')
 
 if __name__ == '__main__':
     object = Solution()
