0193de48c2b0032d3234c1e89cedf9c4a405f1ad
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-05-06 10:58:45 +1000

    Update 497_RandomPointInNonOverlappingRectangles.py

--- 497_RandomPointInNonOverlappingRectangles.py
+++ 497_RandomPointInNonOverlappingRectangles.py
@@ -26,6 +26,39 @@ class Solution(object):
         # imange the area is a matrix of n*m : n= x2-x1+1 m = y2-y1+1
         # the point is kind of [i][j]
         return [x1 + n % (x2 - x1 + 1), y1 + n / (x2 - x1 + 1)]
+    
+    
+    
+ import random
+class Solution:
+    # Runtime: 225 ms, faster than 57.24% of Python3 online submissions for Random Point in Non-overlapping Rectangles.
+    # Memory Usage: 18.8 MB, less than 12.50% of Python3 online submissions for Random Point in Non-overlapping Rectangles.
+    # time: o(n) space: o(n)
+    def __init__(self, rects: list[list[int]]):
+        self._rects = rects
+        self._start = []
+        self._count = 0
+        for v in rects:
+            self._start.append(self._count)
+            self._count += (v[2] - v[0] + 1) * (v[3] - v[1] + 1)
+        print('self._start: {} , self._count: {}'
+              .format(self._start, self._count))
+
+    def pick(self) -> list[int]:
+        target = random.randint(0, self._count-1)
+        i = 0
+        for i in range(len(self._start)-1, -1, -1):
+            print('i: {} self._start[i]: {}'.format(i, self._start[i]))
+            if self._start[i] <= target:
+                break
+
+        pos = target - self._start[i] + 1
+        print('i: {}, target: {} pos: {}'.format(i, target, pos))
+        r, c = divmod(pos, self._rects[i][2]-self._rects[i][0] +1)
+        print('x: {}, y: {}, mod: {}'.format(r, c, self._rects[i][2]-self._rects[i][0] +1))
+        return [self._rects[i][0] + c -1, self._rects[i][1] + r] if c != 0 else [self._rects[i][2], self._rects[i][1] + r -1]
+
+
 
 
 
