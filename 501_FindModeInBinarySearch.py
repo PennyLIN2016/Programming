548730a0a849ff24acf12943a48c9a81d74c6b12
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-05-09 10:34:46 +1000

    Update 501_FindModeInBinarySearch.py

--- 501_FindModeInBinarySearch.py
+++ 501_FindModeInBinarySearch.py
@@ -30,6 +30,53 @@ class Solution(object):
         self.count[root.val]+=1
         self.inOrder(root.right)
 
+    def findMode1(self, root:TreeNode)-> list[int]:
+        # Runtime: 97 ms, faster than 25.96% of Python3 online submissions for Find Mode in Binary Search Tree.
+        # Memory Usage: 18.5 MB, less than 41.95% of Python3 online submissions for Find Mode in Binary Search Tree.
+        # brutal recursion solution space: o(n) time: (n)
+        def dfs(curNode):
+            if curNode.val not in freq:
+                freq[curNode.val] = 1
+            else:
+                freq[curNode.val] += 1
+            if curNode.left:
+                dfs(curNode.left)
+            if curNode.right:
+                dfs(curNode.right)
 
+        freq = {}
+        dfs(root)
+        res = []
+        target = max(freq.values())
+        for k in freq:
+            if freq[k] == target:
+                res.append(k)
+        return res
+
+    def findMode2(self, root:TreeNode)-> list[int]:
+        # Runtime: 53 ms, faster than 93.92% of Python3 online submissions for Find Mode in Binary Search Tree.
+        # Memory Usage: 17.9 MB, less than 92.33% of Python3 online submissions for Find Mode in Binary Search Tree.
+        # no extra space(except for recursion stack) solution: space: O(1) time: o(n): node number
+        # make use of the existing order series in bst
+        res = []
+        p, pre = root, None
+        stack = []
+        maxCnt, cnt = 0, 1
+        while stack or p:
+            while p:
+                stack.append(p)
+                p = p.left
+            p = stack.pop()
+            if pre:
+                cnt = cnt+1 if p.val == pre.val else 1
+            if cnt >= maxCnt:
+                if cnt > maxCnt:
+                    res = [p.val]
+                    maxCnt = cnt
+                else:
+                    res.append(p.val)
+            pre = p
+            p = p.right
+        return res
 
 
