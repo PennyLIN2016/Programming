34ea8b7b5424e88e983a9e600955eb996559e1b4
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-05-12 12:30:03 +1000

    Update 504_Base7.py

--- 504_Base7.py
+++ 504_Base7.py
@@ -20,7 +20,21 @@ class Solution(object):
             res=flag+str(num)+res
         return res if res!='' else "0"
 
-
+    def convertToBase7(self, num: int) -> str:
+        # Runtime: 29 ms, faster than 91.35% of Python3 online submissions for Base 7.
+        # Memory Usage: 13.9 MB, less than 12.53% of Python3 online submissions for Base 7.
+        # time: o(lg7) space: o(1)
+        if num == 0 : return '0'
+        res = ''
+        flag = 1 if num >= 0 else 0
+        num = abs(num)
+        while num > 0:
+            quotient, reminder = divmod(num, 7)
+            res = str(reminder) + res
+            num = quotient
+            #print('num: {} reminder: {}'.format(num, reminder))
+        return res if flag == 1 else '-' + res
+    
 if __name__ == '__main__':
     object = Solution()
     A= -13
