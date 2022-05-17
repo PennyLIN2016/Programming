ec5dca1190d523ee30454702f1ff0d6e34a85a96
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-05-18 09:59:37 +1000

    Update 516_LongestPalindromicSubsequence.py

--- 516_LongestPalindromicSubsequence.py
+++ 516_LongestPalindromicSubsequence.py
@@ -20,6 +20,38 @@ class Solution(object):
                     dp[i][j]=max(dp[i+1][j],dp[i][j-1])
                 print dp
         return dp[0][-1]
+    
+    class Solution:
+    def longestPalindromeSubseq1(self, s: str) -> int:
+        # timeout: 61 / 86 test cases passed.
+        # force recursion solution
+        def getLongest(i, j):
+            if i > j: return 0
+            if i == j: return 1
+            if s[i] == s[j]:
+                return getLongest(i+1, j-1) + 2
+            return max(getLongest(i+1, j), getLongest(i, j-1))
+        return getLongest(0, len(s)-1)
+
+    def longestPalindromeSubseq(self, s: str) -> int:
+        # Runtime: 1417 ms, faster than 80.40% of Python3 online submissions for Longest Palindromic Subsequence.
+        # Memory Usage: 30.7 MB, less than 64.24% of Python3 online submissions for Longest Palindromic Subsequence.
+        # dp solution: time: o(n**2) space: o(n**2)
+        l = len(s)
+        # dp[i][j]: the longest subseq lenght of sub s[i:j+1]
+        dp = [[0] * l for _ in range(l)]
+        #
+        for i in range(l-1, -1,-1):
+            dp[i][i] = 1
+            for j in range(i+1, l):
+                if s[i] == s[j]:
+                    # the longest subseq can extend two chars.
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    # skip one of the two chars
+                    # the recursion relationship have to travel the i from the end.
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][l-1]
 
 
 
