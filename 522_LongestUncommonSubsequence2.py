efb318f3a16b641ffb1a3750a7b663c0b9575a8a
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-05-24 10:51:00 +1000

    Update 522_LongestUncommonSubsequence2.py

--- 522_LongestUncommonSubsequence2.py
+++ 522_LongestUncommonSubsequence2.py
@@ -28,7 +28,30 @@ class Solution(object):
                 return len(value)
         return -1
 
-
+    # Runtime: 51 ms, faster than 56.57% of Python3 online submissions for Longest Uncommon Subsequence II.
+    # Memory Usage: 13.9 MB, less than 77.71% of Python3 online submissions for Longest Uncommon Subsequence II.
+    # force solution: time: o(m*n**2) space: o(1)
+    def findLUSlength(self, strs: list[str]) -> int:
+        # s1 is sub of s2
+        def isSub(s1, s2):
+            pos = 0
+            for char in s2:
+                if char == s1[pos:pos+1]:
+                    pos += 1
+            return pos == len(s1)
+        strs.sort(key=len, reverse=True)
+        for i in range(len(strs)):
+            # not a sub string of others
+            flag = True
+            for j in range(len(strs)):
+                if i == j:
+                    continue
+                if isSub(strs[i], strs[j]):
+                    flag = False
+                    break
+            if flag:
+                return len(strs[i])
+        return -1
 
 
 if __name__ == '__main__':
