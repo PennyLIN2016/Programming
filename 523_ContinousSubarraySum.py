486e6853dd76cba3fad407f66d71233a9cc96661
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-05-25 21:15:29 +1000

    Update 523_ContinousSubarraySum.py

--- 523_ContinousSubarraySum.py
+++ 523_ContinousSubarraySum.py
@@ -34,7 +34,71 @@ class Solution(object):
             # the len>=2 : sum of [m+1:i] %k ==0
             elif dmap[m]+1<i: return True
         return False
-
+    def checkSubarraySum1(self, nums: list[int], k: int) -> bool:
+        # timeout: 94 / 95 test cases passed.
+        # time: o(n*k) space: o(n *k)
+        if len(nums) < 2:
+            return False
+        if k == 1:
+            return True
+        dp = [set() for _ in range(len(nums))]
+        dp[0].add(nums[0] % k)
+        for i in range(1, len(nums)):
+            cur = nums[i] % k
+            if (k-cur) % k in dp[i-1]:
+                return True
+            else:
+                dp[i].add(cur)
+                for v in dp[i-1]:
+                    r = (cur + v) % k
+                    if r not in dp[i]:
+                        dp[i].add(r)
+        return False
+    def checkSubarraySum2(self, nums: list[int], k: int) -> bool:
+        # timeout: 94 / 95 test cases passed.
+        # time: o(n*k) space: o(1)
+        if len(nums) < 2:
+            return False
+        if k == 1:
+            return True
+        preSet = set()
+        preSet.add(nums[0] % k)
+        for i in range(1, len(nums)):
+            cur = nums[i] % k
+            curSet = set()
+            #print('nums[i]: {} cur: {}'.format(nums[i], cur))
+            if (k-cur) % k in preSet:
+                return True
+            else:
+                curSet.add(cur)
+                for v in preSet:
+                    r = (cur + v) % k
+                    if r not in curSet:
+                        curSet.add(r)
+            preSet = curSet
+        return False
+    
+    def checkSubarraySum(self, nums: list[int], k: int) -> bool:
+        # Runtime: 1223 ms, faster than 38.87% of Python3 online submissions for Continuous Subarray Sum.
+        # Memory Usage: 34.4 MB, less than 5.05% of Python3 online submissions for Continuous Subarray Sum.
+        # time: o(n) space: (n)
+        # sum of [:i]
+        pre = [0]
+        cur = 0
+        for v in nums:
+            cur += v
+            pre.append(cur)
+        # reminderDict[r]: the first pos of the reminder 
+        reminderDict = {}
+        for i in range(len(pre)):
+            r = pre[i] % k
+            if r not in reminderDict:
+                reminderDict[r] = i
+            else:
+                # sum of [reminderDict[r]: i] is a match
+                if i - reminderDict[r] >= 2:
+                    return True
+        return False
 
 
 if __name__ == '__main__':
