4b22e700dbf4c3eb2fda5d41c1428553f7e91714
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-05-30 22:06:08 +1000

    Update 528_RandomPickWithWeight.py

--- 528_RandomPickWithWeight.py
+++ 528_RandomPickWithWeight.py
@@ -47,6 +47,48 @@ class Solution(object):
         tmp= random.randint(1,self.weight[-1])
         # bisect.bisect_left in a sorted list find the insert position (in front of the position ) time : o(lgn)--  binary search
         return bisect.bisect_left(self.weight,tmp)
+    
+    import bisect
+import random
+class Solution1:
+    # Runtime: 9681 ms, faster than 5.00% of Python3 online submissions for Random Pick with Weight.
+    # Memory Usage: 17.6 MB,less than 99.66% of Python3 online submissions for Random Pick with Weight.
+    # time:o(n) space:o(n)
+    def __init__(self, w: list[int]):
+        tmpSum = sum(w)
+        self._freList = [0.0]* (len(w))
+        pre = 0.0
+        for i, v in enumerate(w):
+            self._freList[i] = pre + float(v/tmpSum)
+            pre = self._freList[i]
+
+        print('self._freList: {}'.format(self._freList))
+
+
+    def pickIndex(self) -> int:
+        if len(self._freList) == 1:
+            return 0
+        pos = random.random()
+        print('pos: {}'.format(pos))
+        i = 0
+        while True:
+            if self._freList[i] >= pos:
+                return i
+            i += 1
+import bisect
+class Solution:
+    #Runtime: 288 ms, faster than 62.96% of Python3 online submissions for Random Pick with Weight.
+    #Memory Usage: 18.4 MB, less than 78.84% of Python3 online submissions for Random Pick with Weight.
+    def __init__(self, w: list[int]):
+        self._sum = [w[0]]
+        for v in w[1:]:
+            self._sum.append(self._sum[-1] + v)
+
+    def pickIndex(self) -> int:
+        # Similar to bisect_left(), but returns an insertion point which comes after
+        # (to the right of) any existing entries of x in a.
+        # time: o(lgn) space: o(n)
+        return bisect.bisect(self._sum, self._sum[-1]* random.random())
 
 
 # Your Solution object will be instantiated and called as such:
