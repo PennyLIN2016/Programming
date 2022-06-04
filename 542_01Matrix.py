4221ed58efc5860ade4d36a29614556f8794a497
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-04 10:53:20 +1000

    Update 542_01Matrix.py

--- 542_01Matrix.py
+++ 542_01Matrix.py
@@ -35,7 +35,47 @@ class Solution(object):
                 matrix[i][j]=0
             undoQueue=nq
         return res
+    def updateMatrix1(self, mat: list[list[int]]) -> list[list[int]]:
+        # Runtime: 726 ms, faster than 76.94% of Python3 online submissions for 01 Matrix.
+        # Memory Usage: 16.8 MB, less than 90.83% of Python3 online submissions for 01 Matrix.
+        # BFS solution: time: (m*n*4) = (m*n) space: (m*n) the worse scenario for visit
+        m, n = len(mat), len(mat[0])
+        dirs = [(-1,0), (1,0), (0, -1), (0,1)]
+        visit = collections.deque()
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0:
+                    visit.append((r, c))
+                else:
+                    mat[r][c] = float('inf')
+        while visit:
+            r, c = visit.popleft()
+            for x, y in dirs:
+                i, j = r + x, c + y
+                if 0 <= i < m and 0 <= j < n and mat[i][j] > mat[r][c] + 1:
+                    visit.append((i, j))
+                    mat[i][j] = mat[r][c] + 1
+        return mat
 
+    def updateMatrix(self, mat: list[list[int]]) -> list[list[int]]:
+        # 动态规划，从左上角向右下角进行动态规划，再从右小角向左上角动态规划
+        # Runtime: 553 ms, faster than 97.61% of Python3 online submissions for 01 Matrix.
+        # Memory Usage: 16.5 MB, less than 98.46% of Python3 online submissions for 01 Matrix.
+        # dp solution: time: o(n*m) space: o(1)
+        m, n = len(mat), len(mat[0])
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] > 0:
+                    up = mat[r-1][c] if r >0 else float('inf')
+                    left = mat[r][c-1] if c > 0 else float('inf')
+                    mat[r][c] = min(up + 1, left + 1)
+        for r in range(m-1, -1, -1):
+            for c in range(n-1, -1, -1):
+                if mat[r][c] > 0:
+                    down = mat[r+1][c] if r < m - 1 else float('inf')
+                    right = mat[r][c+1] if c < n-1 else float('inf')
+                    mat[r][c] = min(mat[r][c], down + 1, right+ 1)
+        return mat
 if __name__ == '__main__':
     object = Solution()
     n1=[[0,0,0],
