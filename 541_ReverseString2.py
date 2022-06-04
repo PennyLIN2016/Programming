a1e11f494631f0d47d8ca4614184089a2f1b11eb
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-04 10:52:12 +1000

    Update 541_ReverseString2.py

--- 541_ReverseString2.py
+++ 541_ReverseString2.py
@@ -15,6 +15,32 @@ class Solution(object):
             else:
                 tmpL[i:len(tmpL)]=reversed(tmpL[i:len(tmpL)])
         return "".join(tmpL)
+    
+        def reverseStr(self, s: str, k: int) -> str:
+        #Runtime: 53 ms, faster than 36.93% of Python3 online submissions for Reverse String II.
+        #Memory Usage: 14.1 MB, less than 57.30% of Python3 online submissions for Reverse String II.
+        # time: o(n) space: o(1)
+
+        def reverseSubStr(left, right):
+            print('left: {}, right: {}'.format(left, right))
+            while left < right:
+                sList[left], sList[right] = sList[right], sList[left]
+                left += 1
+                right -= 1
+
+        sList = list(s)
+        tail = len(sList)
+        print('tail: {} sList:{}'.format(tail, sList))
+        for start in range(0, tail, 2*k):
+            if tail -1 < start + k -1:
+                print('tail-1: {} start:{}'.format(tail-1, start))
+                reverseSubStr(start, tail-1)
+            else:
+                print('end: {} start:{}'.format(start + k -1, start))
+                reverseSubStr(start, start + k -1)
+
+        return ''.join(sList)
+
 
 if __name__ == '__main__':
     object = Solution()
