b9e97f46f55532065f115d58ea2c486fc13d076a
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-05 18:13:51 +1000

    Update 547_FriendCircles.py

--- 547_FriendCircles.py
+++ 547_FriendCircles.py
@@ -23,6 +23,46 @@ class Solution(object):
                 res+=1
                 findFriend(M,c,n)
         return res
+    def findCircleNum1(self, isConnected: list[list[int]]) -> int:
+        # Runtime: 297 ms, faster than 39.20% of Python3 online submissions for Number of Provinces.
+        # Memory Usage: 14.3 MB, less than 76.99% of Python3 online submissions for Number of Provinces.
+        # bfs + iteration time: O(n**2) space: o(n)
+        leftCity = set([i for i in range(len(isConnected))])
+        res = 0
+        while leftCity:
+            res += 1
+            city = list(leftCity)[-1]
+            leftCity.remove(city)
+            stack = [city]
+            while stack:
+                tmp = []
+                for start in stack:
+                    for c in list(leftCity):
+                        if isConnected[start][c] == 1:
+                            leftCity.remove(c)
+                            tmp.append(c)
+                stack = tmp
+        return res
+
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
+        # Runtime: 216 ms, faster than 74.80% of Python3 online submissions for Number of Provinces.
+        # Memory Usage: 14.7 MB, less than 28.71% of Python3 online submissions for Number of Provinces.
+        # dfs+recursion solution: time: o(n*n/2) space: o(n)
+        def dfs(index):
+            visited[index] = 1
+            for i in range(len(isConnected)):
+                if i == index:
+                    continue
+                if isConnected[index][i] == 1 and visited[i] == 0:
+                    dfs(i)
+
+        visited = [0] * len(isConnected)
+        res = 0
+        for n in range(len(isConnected)):
+            if visited[n] == 0:
+                res += 1
+                dfs(n)
+        return res
 
 
 
