5bc3195e1a963dae810cb80702e3ac95e4a7a446
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-06 14:27:08 +1000

    Update 551_StudentAttendanceRecord1.py

--- 551_StudentAttendanceRecord1.py
+++ 551_StudentAttendanceRecord1.py
@@ -16,6 +16,23 @@ class Solution(object):
                 count+=1
             if count>=2:return False
         return True
+    
+        def checkRecord(self, s: str) -> bool:
+        # Runtime: 33 ms, faster than 83.82% of Python3 online submissions for Student Attendance Record I.
+        # Memory Usage: 13.8 MB, less than 54.87% of Python3 online submissions for Student Attendance Record I.
+        # time: o(n) space: o(1)
+        if len(s) < 2:
+            return True
+        absent = 0
+        for i, c in enumerate(s):
+            if c == 'A':
+                absent += 1
+                if absent == 2:
+                    return False
+            if i > 1 and c == 'L':
+                if s[i-1] == s[i-2] == 'L':
+                    return False
+        return True
 
 if __name__ == '__main__':
     object = Solution()
