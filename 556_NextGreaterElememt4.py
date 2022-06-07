fea91c15df4ad62d1c607de1d3444392b170261d
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-07 21:50:06 +1000

    Update 556_NextGreaterElememt4.py

--- 556_NextGreaterElememt4.py
+++ 556_NextGreaterElememt4.py
@@ -28,6 +28,53 @@ class Solution(object):
     def reverse(self,nums,i,j):
         for k in range(i, (i + j) / 2 + 1):
             self.swap(nums, k, i + j - k)
+    def nextGreaterElement1(self, n: int) -> int:
+        # Runtime: 60 ms, faster than 9.71% of Python3 online submissions for Next Greater Element III.
+        # Memory Usage: 13.8 MB, less than 77.31% of Python3 online submissions for Next Greater Element III.
+        # time: o(n) space: o(1)
+        nums = list(str(n))
+        l = len(nums)
+        for i in range(l-1, -1, -1):
+            if i > 0 and nums[i-1] < nums[i]:
+                if i == l-1 or nums[-1] == nums[i]:
+                    nums[i-1], nums[i] = nums[i], nums[i-1]
+                    res = int(''.join(nums))
+                    return -1 if res > 0x7FFFFFFF else res
+                else:
+                    for j in range(l-1, i-1, -1):
+                        if nums[j] > nums[i-1]:
+                            nums[i - 1], nums[j] = nums[j], nums[i - 1]
+                            break
+                    res = int(''.join(nums[:i]+nums[i:][::-1]))
+                return res if res <= 0x7FFFFFFF else -1
+        return -1
+
+    def nextGreaterElement(self, n: int) -> int:
+        # 从右到左找到第一个降序的数字, index记为i-1, 然后从右到左找到第一个比这个数字大的数字,
+        # index记为j, 交换两个数字的值, 然后将i-1之后的序列倒序排列即可. 当降序的数字不存在时,
+        # i==0, 返回-1. 当得到的结果超过32-bit时, 也返回-1.
+        # Runtime: 42 ms, faster than 45.18% of Python3 online submissions for Next Greater Element III.
+        # Memory Usage: 14 MB, less than 25.16% of Python3 online submissions for Next Greater Element III.
+        # time: o(n) space: o(n)
+        nums = list(str(n))
+
+        i = len(nums) - 1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+
+        # nums[i-1] < nums[i]
+        if i == 0: return -1
+
+        j = len(nums) - 1
+        while nums[j] <= nums[i-1]:
+            j -= 1
+        # nums[j] > nums[i-1]
+        nums[j], nums[i-1] = nums[i-1], nums[j]
+        nums[i:] = nums[i:][::-1]
+        res = int(''.join(nums))
+        if res > 2**31 -1:
+            return -1
+        return res
 
 
 if __name__ == '__main__':
