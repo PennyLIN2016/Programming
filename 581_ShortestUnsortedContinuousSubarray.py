7b56b4e453d71c03644a28b9b4354c421fb526e2
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-15 19:40:56 +1000

    Update 581_ShortestUnsortedContinuousSubarray.py

--- 581_ShortestUnsortedContinuousSubarray.py
+++ 581_ShortestUnsortedContinuousSubarray.py
@@ -15,6 +15,28 @@ class Solution(object):
         while j>=0 and nums[j] == sortedList[j]:
             j-=1
         return j-i+1
+    
+    ##### python 3 solution:
+     def findUnsortedSubarray(self, nums: list[int]) -> int:
+        # Runtime: 314 ms, faster than 43.94% of Python3 online submissions for Shortest Unsorted Continuous Subarray.
+        # Memory Usage: 15.2 MB, less than 50.53% of Python3 online submissions for Shortest Unsorted Continuous Subarray.
+        # time: o(nlgn) space: o(n)
+        oldNums = nums[:]
+        nums.sort()
+        left, right = 0, len(nums)-1
+        while left <= right and oldNums[left] == nums[left]:
+            left += 1
+        if left == right+1:
+            return 0
+        while right >= left and oldNums[right] == nums[right]:
+            right -= 1
+        return right - left + 1
+
+    def findUnsortedSubarray(self, nums: list[int]) -> int:
+        #Runtime: 178 ms, faster than 100.00% of Python3 online submissions for Shortest Unsorted Continuous Subarray.
+        #Memory Usage: 15.4 MB, less than 6.70% of Python3 online submissions for Shortest Unsorted Continuous Subarray.
+        order = [x == y for x, y in zip(nums, sorted(nums))]
+        return 0 if all(order) else (len(nums)) - order.index(False) - order[::-1].index(False)
 
 if __name__ == '__main__':
     object = Solution()
