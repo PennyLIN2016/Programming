e767b5ae598e7283bf2748e37ab7ad515f78a729
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-16 12:33:49 +1000

    Update 592_FractionAdditionAndSubtraction.py

--- 592_FractionAdditionAndSubtraction.py
+++ 592_FractionAdditionAndSubtraction.py
@@ -42,6 +42,44 @@ class Solution(object):
     def lcm(self,a,b):
         return a*b /self.gcd(a,b)
 
+    ### python3
+        def fractionAddition1(self, expression: str) -> str:
+        # Runtime: 44 ms, faster than 47.95% of Python3 online submissions for Fraction Addition and Subtraction.
+        # Memory Usage: 13.9 MB, less than 80.70% of Python3 online submissions for Fraction Addition and Subtraction.
+        # math solution eval; o(len(expression)) space: o(1)
+        import fractions
+        v = fractions.Fraction(eval(expression)).limit_denominator().as_integer_ratio()
+        return '{}/{}'.format(v[0], v[1])
+
+    def fractionAddition(self, expression: str) -> str:
+        #Runtime: 66 ms, faster than 11.11% of Python3 online submissions for Fraction Addition and Subtraction.
+        # Memory Usage: 13.9 MB, less than 80.70% of Python3 online submissions for Fraction Addition and Subtraction.
+
+        import functools
+        def gcd(a, b):
+            if a < b:
+                a, b = b, a
+            while b:
+                a, b = b, a%b
+            return a
+        def lcm(a, b):
+            return a*b / gcd(a,b)
+
+        part = ''
+        fractions = []
+        for s in expression:
+            # get a fraction number
+            if s in '+-':
+                if part: fractions.append(part)
+                part = ''
+            part += s
+        if part: fractions.append(part)
+        numerators = [int(v.split('/')[0]) for v in fractions]
+        denumerators = [int(v.split('/')[1]) for v in fractions]
+        n2 = functools.reduce(lcm, denumerators)
+        n1 = sum(h*n2/l for h, l in zip(numerators, denumerators))
+        m = abs(gcd(n1, n2))
+        return '{}/{}'.format(int(n1/m), int(n2/m))
 
 if __name__ == '__main__':
     object = Solution()
