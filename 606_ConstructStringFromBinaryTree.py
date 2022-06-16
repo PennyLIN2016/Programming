d4b877a40962d0931b0b8335b313427fff5d0d76
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-16 21:47:59 +1000

    Update 606_ConstructStringFromBinaryTree.py

--- 606_ConstructStringFromBinaryTree.py
+++ 606_ConstructStringFromBinaryTree.py
@@ -14,6 +14,22 @@ class Solution:
         left= '({})'.format(self.tree2str(t.left)) if t.left or t.right else ''
         right= '({})'.format(self.tree2str(t.right)) if t.right else ''
         return '{}{}{}'.format(t.val,left,right)
+     
+  #############python3
+    def tree2str(self, root: TreeNode) -> str:
+        # Runtime: 62 ms, faster than 71.27% of Python3 online submissions for Construct String from Binary Tree.
+        # Memory Usage: 16.8 MB, less than 5.70% of Python3 online submissions for Construct String from Binary Tree.
+        # time: o(n) space: o(n)
+        if not root:
+            return ""
+        s1 = self.tree2str(root.left)
+        s2 = self.tree2str(root.right)
+        res = str(root.val)
+        # if s1= '' and s2 != '' need to add () to mean there is an empty left tree
+        if s1 or s2: res += '({})'.format(s1)
+        if s2 != '': res += '({})'.format(s2)
+        return res
+
 
 
 if __name__ == '__main__':
