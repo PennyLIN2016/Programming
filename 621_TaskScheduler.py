4e960f96ba10f6100d117b88712b308c18c2eb1a
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-17 11:39:21 +1000

    Update 621_TaskScheduler.py

--- 621_TaskScheduler.py
+++ 621_TaskScheduler.py
@@ -23,6 +23,29 @@ class Solution(object):
         print(keyCount)
         res= (n+1)*(maxCount-1)+ len(keyCount[maxCount])
         return max(res,len(tasks))
+    
+    
+    #############python3
+    def leastInterval(self, tasks: list[str], n: int) -> int:
+        # Runtime: 435 ms, faster than 91.35% of Python3 online submissions for Task Scheduler.
+        # Memory Usage: 14.2 MB, less than 99.21% of Python3 online submissions for Task Scheduler.
+        # time: o(n + n + n) = o(n) space: o(n)
+        if n == 0:
+            return len(tasks)
+        dic ={}
+        for i in tasks:
+            if i in dic:
+                dic[i] += 1
+            else:
+                dic[i] = 1
+
+
+        maxF = max(list(dic.values()))
+        cnt = 0
+        for v in dic.values():
+            if v == maxF:
+                cnt += 1
+        return max((maxF-1)*(n+1) + cnt, len(tasks))
 
 if __name__ == '__main__':
     object = Solution()
