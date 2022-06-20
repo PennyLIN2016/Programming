0d3486c47ebc728a4cb7c758729f057245d96a0e
PennyLIN2016 <34665813+PennyLIN2016@users.noreply.github.com>
2022-06-20 18:56:07 +1000

    Update 628_MaximumProductOfThreeNumbers.py

--- 628_MaximumProductOfThreeNumbers.py
+++ 628_MaximumProductOfThreeNumbers.py
@@ -13,7 +13,17 @@ class Solution(object):
         r2=nums[-1]*nums[0]*nums[1]
         return max(r1,r2)
 
-
+#### python3
+    def maximumProduct(self, nums: list[int]) -> int:
+        # Runtime: 382 ms, faster than 42.71% of Python3 online submissions for Maximum Product of Three Numbers.
+        # Memory Usage: 15.2 MB, less than 20.71% of Python3 online submissions for Maximum Product of Three Numbers.
+        # time: o(nlgn) space: o(1)
+        nums.sort()
+        if nums[0] >= 0 or nums[-1] <= 0: return nums[-1] * nums[-2] * nums[-3]
+        if nums[1] < 0:
+            return max(nums[-1] * nums[-2] * nums[-3],  nums[-1] * nums[0] * nums[1])
+        else:
+            return nums[-1] * nums[-2] * nums[-3]
 
 if __name__ == '__main__':
     object = Solution()
